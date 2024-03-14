0fca7bba9611ebf66a99e0cafc7a4e1034cd3608
Dzmitry Papkou <papkoud@gmail.com>
2024-03-14 16:31:50 +0000

    Merged in initial-spring-setup (pull request #2)
    * Initialized the Spring project, added starter configurations for easier operation
    
    * Configure environment variables for database
    
    * Configure build to generate single executable .jar file
    
    Approved-by: Paulius Preikša
    Approved-by: Gvidas Gaidauskas

---
+++ src/main/java/com/interview/manager/backend/configurations/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.interview.manager.backend.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
+                        .anyRequest().authenticated())
+
+                .formLogin(formLogin -> formLogin
+                        .loginProcessingUrl("/login").permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/logout").permitAll());
+
+        return http.build();
+    }
+
+}
