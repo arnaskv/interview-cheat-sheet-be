1c1642b677f524f813a9f55f5035467387e9d986
kasparas.putrius <kasparas.putrius@gmail.com>
2024-03-28 17:44:15 +0200

    GSF2024S-1 Get interview questions: - Full imports

--- src/main/java/com/interview/manager/backend/entities/InterviewQuestion.java
+++ src/main/java/com/interview/manager/backend/entities/InterviewQuestion.java
@@ -1,6 +1,10 @@
 package com.interview.manager.backend.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
