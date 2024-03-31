9040bd1e661e36d0b19d30093fbb6417d2f6c56d
kasparas.putrius <kasparas.putrius@gmail.com>
2024-03-31 18:35:38 +0300

    GSF2024S-1 Get interview questions: - changed how controllers are constructed and their return types

--- src/main/java/com/interview/manager/backend/services/interviewQuestion/InterviewQuestionService.java
+++ src/main/java/com/interview/manager/backend/services/interviewQuestion/InterviewQuestionService.java
@@ -6,16 +6,25 @@ import com.interview.manager.backend.repositories.InterviewQuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class InterviewQuestionService {
     private final InterviewQuestionRepository interviewQuestionRepository;
 
-    public InterviewQuestionResponseDto findById(Long interviewId) {
-        InterviewQuestion interviewQuestion = interviewQuestionRepository.findById(interviewId).get();
-        return InterviewQuestionResponseDto.builder()
-                .id(interviewQuestion.getId())
-                .title(interviewQuestion.getTitle())
-                .build();
+    public Optional<InterviewQuestionResponseDto> findById(Long interviewId) {
+        Optional<InterviewQuestion> optionalInterviewQuestion = interviewQuestionRepository.findById(interviewId);
+        return optionalInterviewQuestion.map(InterviewQuestionResponseDto::of);
+    }
+
+    public List<InterviewQuestionResponseDto> getAllInterviewQuestions() {
+        return interviewQuestionRepository
+                .findAll()
+                .stream()
+                .map(InterviewQuestionResponseDto::of)
+                .collect(Collectors.toList());
     }
 }
