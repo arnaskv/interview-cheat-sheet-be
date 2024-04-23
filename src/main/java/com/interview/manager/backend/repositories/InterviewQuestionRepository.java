be01de6ecf973b7891864b9a5eb8384ac6663593
Tomas Miliūnas <tomasmiliunas14@gmail.com>
2024-04-23 10:19:00 +0300

    Removed unused imports and comments

--- src/main/java/com/interview/manager/backend/repositories/InterviewQuestionRepository.java
+++ src/main/java/com/interview/manager/backend/repositories/InterviewQuestionRepository.java
@@ -2,19 +2,9 @@ package com.interview.manager.backend.repositories;
 
 import com.interview.manager.backend.models.InterviewQuestion;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface InterviewQuestionRepository extends JpaRepository<InterviewQuestion, Long> {
-
-//    @Query(value = """
-//            select q.id, q.title, c.title as category
-//            from interview_questions q
-//            inner join interview_categories c on q.category_id = c.id
-//            """, nativeQuery = true)
-//    List<InterviewQuestion> findAllQuestions();
 
 }
