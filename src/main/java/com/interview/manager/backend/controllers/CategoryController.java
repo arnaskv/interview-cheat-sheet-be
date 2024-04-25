4e6b55b92c8e1cec542fe31f732190c01490512e
Artem Maslii <maslijartei@gmail.com>
2024-04-25 12:04:30 +0300

    Refactor: remove unused import

--- src/main/java/com/interview/manager/backend/controllers/CategoryController.java
+++ src/main/java/com/interview/manager/backend/controllers/CategoryController.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
