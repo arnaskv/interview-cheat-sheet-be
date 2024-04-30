660a0011fce3a8418d771eb3ac4a3778cfd54652
Artem Maslii <maslijartei@gmail.com>
2024-04-30 14:54:59 +0300

    Add: subquestion list, which displays unless it's empty

--- src/main/java/com/interview/manager/backend/dto/InterviewQuestionResponseDto.java
+++ src/main/java/com/interview/manager/backend/dto/InterviewQuestionResponseDto.java
@@ -1,11 +1,16 @@
 package com.interview.manager.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.interview.manager.backend.models.Category;
 import com.interview.manager.backend.models.InterviewQuestion;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,12 +21,20 @@ public class InterviewQuestionResponseDto {
     private Long id;
     private String title;
     private Category category;
+    @JsonInclude(Include.NON_EMPTY)
+    private List<InterviewQuestionResponseDto> subQuestions;
 
     public static InterviewQuestionResponseDto of(InterviewQuestion interviewQuestion) {
+        List<InterviewQuestionResponseDto> subQuestionsDto = interviewQuestion.getSubQuestions().stream()
+            .map(InterviewQuestionResponseDto::of)
+            .collect(Collectors.toList());
+
         return InterviewQuestionResponseDto.builder()
-                .id(interviewQuestion.getId())
-                .title(interviewQuestion.getTitle())
-                .category(interviewQuestion.getCategory())
-                .build();
+            .id(interviewQuestion.getId())
+            .title(interviewQuestion.getTitle())
+            .category(interviewQuestion.getCategory())
+            .subQuestions(subQuestionsDto)
+            .build();
     }
+
 }
--- src/main/java/com/interview/manager/backend/models/InterviewQuestion.java
+++ src/main/java/com/interview/manager/backend/models/InterviewQuestion.java
@@ -1,17 +1,23 @@
 package com.interview.manager.backend.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -31,4 +37,11 @@ public class InterviewQuestion {
     @ManyToOne
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private InterviewQuestion parentQuestion;
+
+    @OneToMany(mappedBy = "parentQuestion", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<InterviewQuestion> subQuestions = new ArrayList<>();
 }
