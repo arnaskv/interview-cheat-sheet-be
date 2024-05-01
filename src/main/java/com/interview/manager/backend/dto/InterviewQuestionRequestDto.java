cad1fb358252aabe4faa685102009b82972e92ae
Artem Maslii <maslijartei@gmail.com>
2024-05-01 16:59:57 +0300

    Resolve conflict && merge with be-integration branch

--- src/main/java/com/interview/manager/backend/dto/InterviewQuestionRequestDto.java
+++ src/main/java/com/interview/manager/backend/dto/InterviewQuestionRequestDto.java
@@ -1,5 +1,6 @@
 package com.interview.manager.backend.dto;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -15,6 +16,9 @@ public class InterviewQuestionRequestDto {
     @NotBlank(message = "Title should not be blank")
     @Size(max = 256, min = 1, message = "Title should be between {min} and {max} characters")
     private String title;
+
     private Long categoryId;
+
+    @Nullable
     private Long parentId;
 }
