85a6ed59439f3d35522beefb89bdb3d18cbfbb11
Paulius Preikša <preiksap@gmail.com>
2024-05-08 20:50:36 +0300

    feat: added user db table

---
+++ src/main/java/com/interview/manager/backend/models/User.java
@@ -0,0 +1,53 @@
+package com.interview.manager.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, updatable = false)
+    private String salt;
+
+    @Column(name = "date_created", nullable = false, updatable = false)
+    private OffsetDateTime dateCreated;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.setDateCreated(OffsetDateTime.now());
+    }
+}
